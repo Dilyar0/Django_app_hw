5e74186ec7c0c453c48f37190f6f006a70e40b1e
Dilyar0 <diliartemirbekov01@gmail.com>
2022-04-25 19:21:48 +0600

    home work 3

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
+from django.shortcuts import redirect, reverse
 
 
 def book_all(request):
@@ -10,3 +11,15 @@ def book_all(request):
 def book_detail(request, id):
     book = get_object_or_404(models.Book, id=id)
     return render(request, "book_detail.html", {"book": book})
+
+
+def add_book(request):
+    method = request.method
+    if method == "POST":
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("books:book_all"))
+    else:
+        form = forms.BookForm()
+    return render(request, "add_books.html", {'form': form})
